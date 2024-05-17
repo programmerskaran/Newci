35e263d082b27db754bf701885955d688cacb77d
webgrity158 <webgrity158@gmail.com>
2024-05-17 13:01:20 +0530

    fix error

--- application/views/frontend/layouts/footer.php
+++ application/views/frontend/layouts/footer.php
@@ -63,14 +63,13 @@
       </div>
       <!-- copyright section end -->
       <!-- Javascript files-->
-      <script src="<?php echo base_url(); ?>assets/<?php echo base_url(); ?>assets/js/jquery.min.js"></script>
-      <script src="<?php echo base_url(); ?>assets/<?php echo base_url(); ?>assets/js/popper.min.js"></script>
-      <script src="<?php echo base_url(); ?>assets/<?php echo base_url(); ?>assets/js/bootstrap.bundle.min.js"></script>
-      <script src="<?php echo base_url(); ?>assets/<?php echo base_url(); ?>assets/js/jquery-3.0.0.min.js"></script>
-      <script src="<?php echo base_url(); ?>assets/<?php echo base_url(); ?>assets/js/plugin.js"></script>
-      <!-- sidebar -->
-      <script src="<?php echo base_url(); ?>assets/<?php echo base_url(); ?>assets/js/jquery.mCustomScrollbar.concat.min.js"></script>
-      <script src="<?php echo base_url(); ?>assets/<?php echo base_url(); ?>assets/js/custom.js"></script>
+      <script src="<?php echo base_url(); ?>assets/js/jquery.min.js"></script>
+      <script src="<?php echo base_url(); ?>assets/js/popper.min.js"></script>
+      <script src="<?php echo base_url(); ?>assets/js/bootstrap.bundle.min.js"></script>
+      <script src="<?php echo base_url(); ?>assets/js/jquery-3.0.0.min.js"></script>
+      <script src="<?php echo base_url(); ?>assets/js/plugin.js"></script>
+      <script src="<?php echo base_url(); ?>assets/js/jquery.mCustomScrollbar.concat.min.js"></script>
+      <script src="<?php echo base_url(); ?>assets/js/custom.js"></script>
       <!-- javascript --> 
       <script>
          // Material Select Initialization
